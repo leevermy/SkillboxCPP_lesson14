66951f424c7f642c8f74de44c7cda7f840ca4228
leevermy <risnotr@gmail.com>
2024-05-07 21:01:57 +0300

    third task done

--- task03.cpp
+++ task03.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
-#include <vector>
+#include <cassert>
 
 int main() {
+    int A_matrix[4][4];
+    int B_matrix[4][4];
+
+    std::cout << "Enter matrix A (4x4):\n";
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            std::cin >> A_matrix[i][j];
+        } 
+    }
+
+    std::cout << "\nEnter matrix B (4x4):\n";
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            std::cin >> B_matrix[i][j];
+            assert(B_matrix[i][j] == A_matrix[i][j]);
+            if(i != j) {
+                A_matrix[i][j] = 0;
+            }
+        } 
+    }
+
+    // for (int i = 0; i < 4; ++i) {
+    //     for (int j = 0; j < 4; ++j) {
+    //         if(i != j) {
+    //             A_matrix[i][j] = 0;
+    //         }
+    //     } 
+    // }
+
+    std::cout << "\nMatrices are equal. Transforming matrix A into a diagonal matrix:\n";
+    for (int i = 0; i < 4; ++i) {
+        bool is_space = false;
+        for (int j = 0; j < 4; ++j) {
+            if (is_space) {
+                std:: cout << " ";
+            }
+            std::cout << A_matrix[i][j];
+            is_space = true;
+        }
+        std::cout << std::endl;
+    }
+    
     return 0;
 }
